58b7cd63db0e189a0fd42fa8a448bcd0bf0e80f5
keven-ruiz <kevenruizh@gmail.com>
2021-06-14 01:01:13 -0700

    2 of 5 rob lowes test failing

--- __tests__/how-many-robs.test.js
+++ __tests__/how-many-robs.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Roblowe from '../lib/models/Roblowe.js';
 
 describe('how many rob lowes does it take to rob Lowes', () => {
   beforeEach(() => {
@@ -19,6 +20,18 @@ describe('how many rob lowes does it take to rob Lowes', () => {
       many: 'because Rob Lowe has many',
       times: 'oh, rob lowe',
     });
+  });
+
+  it('finds a rob lowe who tried to rob lowe by id  via GET', async () => {
+    const roblowe = await Roblowe.insert({
+      how: 'many lowes would Rob lowe rob ',
+      many: 'because Rob Lowe has many',
+      times: 'oh, rob lowe',
+    });
+
+    const res = await request(app).get(`/api/v1/roblowes/${roblowe.id}`);
+
+    expect(res.body).toEqual(roblowe);
   });
 
 
